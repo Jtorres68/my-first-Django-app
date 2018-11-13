f06e80827432d65b9f329c6244acabb89690d58c
Jtorres68 <jtorre68@fau.edu>
2018-11-13 17:00:27 -0500

    Added static and post_detail files

--- realestate/urls.py
+++ realestate/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),    #the name is the name of the URL that will be used to identify the view
+    path('post/<int:pk>/', views.post_detail, name='post_detail'), #post/<int:pk>/ specifies a URL pattern such as http://127.0.0.1:8000/post/1/
 ]
--- realestate/views.py
+++ realestate/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 
@@ -7,3 +7,6 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'realestate/post_list.html', {'posts': posts})
 
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk) #primary key found and displays the one blog or 404 page displayed
+    return render(request, 'realestate/post_detail.html', {'post': post})
